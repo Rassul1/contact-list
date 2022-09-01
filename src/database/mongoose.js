16ac01e4bb933ce5fe1c1205883ad476f7536a48
Rassul1 <begenov.rasul@gmail.com>
2022-09-01 14:08:55 +0700

    123

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectionString = process.env.CONNECTION_STRING ||
-"mongodb+srv://Rassul:kotakpas1@cluster0.obg1ht7.mongodb.net/?retryWrites=true&w=majority";
+"mongodb+srv://Rassul:kotakpas1@eucentral1.fcxy3hw.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(value => console.log('Connected to database'))
-    .catch(error => console.log('Error connecting to database: ', error));
+    .catch(error => console.log('Error connecting to database: ', error)); 
